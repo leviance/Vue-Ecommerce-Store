8765fc24f920f355b333992b2a0fccf748d95a85
drdung1999 <duongdung12a8@gmail.com>
2021-05-24 10:36:19 +0700

    complete challenge show value in web page

---
+++ src/views/Other.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <input v-model="data_input" type="text" placeholder="enter the value" />
+    <button v-on:click="toggle_table">show table</button>
+    <div v-show="show_table">
+      <Challenge :arr_input="arr" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Challenge from "../components/challenge/Challenge";
+
+export default {
+  components: {
+    Challenge,
+  },
+
+  data() {
+    return {
+      data_input: "",
+      arr: [],
+      show_table: false,
+    };
+  },
+
+  methods: {
+    toggle_table() {
+      this.show_table = !this.show_table;
+
+      if (this.show_table) {
+        this.arr.push(...this.data_input.split(""));
+        this.data_input = "";
+      } else {
+        this.arr = [];
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
